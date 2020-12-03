5e4de0cd88ab97b64d3e2265a2906b0e8adbb1e6
паша Андалюкевич <elityrepa@hotmail.com>
2020-12-03 19:56:00 +0300

    feat: add homework1

---
+++ src/Andaliukevich.Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Andaliukevich.Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
