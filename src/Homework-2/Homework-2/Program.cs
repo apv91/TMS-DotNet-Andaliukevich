95e9aa74244b5011d912db0f8dfd0b0bf36524c0
Your Name <elityrepa@hotmail.com>
2020-12-10 19:38:29 +0300

    init: add hw2

---
+++ src/Homework-2/Homework-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите дату чтобы узнать день недели: ");
+            string date1 = Console.ReadLine();
+            bool dateweek = DateTime.TryParse(date1, out DateTime date);
+            if (dateweek)
+            {
+                Console.WriteLine(date.DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Неправильно введена дата.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
